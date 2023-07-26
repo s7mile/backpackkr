fc1e576d3449e2474974ffaa69efe648112ceb78
s7mile <haeru29@gmail.com>
2023-07-26 22:52:58 +0900

    add formUI

---
+++ src/components/form/FormItem.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <div class="textarea_wrapper">
+      <textarea
+        name="comment"
+        id="commentTextarea"
+        :placeholder="placeholder"
+        v-model="value"
+        :disabled="disabled"
+        :readonly="readonly"
+        title="코멘트"
+        :maxlength="maxLength"
+      ></textarea>
+      <button
+        :class="
+          defaultValue === value ||
+          (defaultValue === undefined && value.length === 0)
+            ? 'hide'
+            : 'show'
+        "
+      >
+        Save
+      </button>
+    </div>
+    <p>{{ maxLength - value.length }}자</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "FormItem",
+  props: {
+    defaultValue: String,
+    placeholder: String,
+    disabled: Boolean,
+    readonly: Boolean,
+  },
+  setup(props) {
+    const value = ref(props.defaultValue ? props.defaultValue : "");
+    const maxLength = 500;
+
+    return {
+      value,
+      maxLength,
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+.textarea_wrapper:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+
+textarea {
+  float: left;
+  width: $textarea-width;
+  height: 100px;
+  border: 1px solid $border-color;
+  resize: none;
+  overflow: auto;
+  outline: none;
+
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+
+  &:focus {
+    border-color: $main-color;
+  }
+}
+
+span {
+  color: $base-color;
+}
+
+button {
+  float: left;
+  width: 100px;
+  height: 100px;
+  margin-left: 10px;
+  border: 1px solid $border-color;
+  background: #fff;
+  color: $main-color;
+
+  &:disabled {
+    background: #f0f0f0 !important;
+  }
+
+  &:active {
+    background: #f9f9f9;
+  }
+
+  &.show {
+    display: block;
+  }
+
+  &.hide {
+    display: none;
+  }
+}
+
+p {
+  width: $textarea-width;
+  margin-top: 5px;
+  text-align: right;
+  color: $base-color;
+  font-size: 12px;
+}
+</style>
--- src/views/FormUI.vue
+++ src/views/FormUI.vue
@@ -1,13 +1,30 @@
 <template>
   <h2>과제 2. 입력 폼 UI</h2>
-  <ul></ul>
+  <article>
+    <h3>1. 초기값O & 입력 중</h3>
+    <FormItem
+      defaultValue="초기값이 있을 수 있습니다."
+      placeholder="주문요청사항을 입력해주세요"
+    />
+    <h3>2. 초기값X & 입력 중</h3>
+    <FormItem placeholder="주문요청사항을 입력해주세요" />
+    <h3>3. disabled</h3>
+    <FormItem placeholder="주문요청사항을 입력해주세요" disabled />
+    <h3>4. readonly</h3>
+    <FormItem placeholder="주문요청사항을 입력해주세요" readonly />
+  </article>
 </template>
 
 <script lang="ts">
+import FormItem from "../components/form/FormItem.vue";
+
 export default {
   name: "FormUI",
   props: {
     msg: String,
+  },
+  components: {
+    FormItem,
   },
 };
 </script>
